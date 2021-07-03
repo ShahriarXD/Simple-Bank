0efb5d3dba632bb2f67f92fe331923d340e546e7
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2021-07-03 21:24:40 +0600

    Add files via upload

---
+++ a.js
@@ -0,0 +1,14 @@
+var a = document.querySelector("#xnxx");
+a.addEventListener("click",function(){
+  var xx = document.querySelectorAll("div")[0];
+  xx.style.display = "none";
+  var yy = document.querySelectorAll("div")[1]
+  yy.style.display = "block"
+}
+)
+
+// time
+const d = new Date();
+document.getElementById("looool").innerHTML = d;
+
+// time
